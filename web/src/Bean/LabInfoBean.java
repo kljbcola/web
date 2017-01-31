9ea51696b3513a444d7ccbbcfd3f83f9f6592e64
hansir <hansir@hans>
2017-01-31 14:36:25 +0800

    LabinfoBean

---
+++ web/src/Bean/LabInfoBean.java
@@ -0,0 +1,25 @@
+package Bean;
+
+public class LabInfoBean {
+	public String lab_number=null;      //实验室编号
+	public String lab_name=null;        //实验室名称
+	public String faculty_number=null;  //所属院系
+	public String lab_location=null;    //实验室地址
+	public String description=null;     //实验室描述
+	public String build_time=null;      //建立时间
+	public String research_area=null;   //研究领域
+	public String phone=null;           //联系电话
+	public String Email=null;           //负责人电子邮件
+	public String feature=null;         //实验室特色
+	public String owner=null;           //负责人
+	public String price=null;           //价格
+	public String overtime_price=null;  //超时价格
+	public String min_time=null;        //最少使用时间
+	public String max_time=null;        //最多使用时间
+	public String open_hours=null;      //开始时间
+	public String close_hour=null;      //截止时间
+	public String lab_status=null;      //实验室状态
+	public String attachment=null;      //内含设备
+	public String user_type=null;       //实验室权限
+	public String lab_ip=null;          //实验室终端IP地址
+}
